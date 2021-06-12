feaf5737a7a1b5824a123cab523886f4b050bf33
Alex <a.mastianitsa@gmail.com>
2021-06-12 16:45:26 +0300

    second

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const slides = document.querySelectorAll('.slide')
-for (const item of slides) {
-    item.addEventListener('click', () => {
-        clearActiveClasses()
-        item.classList.add('active')
-        
-    })
-}
 
-function clearActiveClasses() {
-    slides.forEach(item => {
-        item.classList.remove('active')
-    })
+function slides(activeClass = 1) {
+    const slides = document.querySelectorAll('.slide')
+    slides[activeClass].classList.add('active')
+    for (const item of slides) {
+        item.addEventListener('click', () => {
+            clearActiveClasses()
+            item.classList.add('active')
+        })
+    }
+    
+    function clearActiveClasses() {
+        slides.forEach(item => {
+            item.classList.remove('active')
+        })
+    }
+          
 }
+slides(2)
